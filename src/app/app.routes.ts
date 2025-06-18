42ff441cf8e640e4efbee3cd326154be2ff0a368
Aula Kharismah <aulakharismah21@gmail.com>
2025-06-18 11:27:08 +0700

    solve error route in production

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
 import { HomeComponent } from './pages/home/home.component';
 import { PageDetailComponent } from './pages/page-detail/page-detail.component';
 import { ListNewsComponent } from './pages/list-news/list-news.component';
+import { getPrerenderParams } from './prerender-params';
 
 export const routes: Routes = [
     {
@@ -13,16 +14,36 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent
       },
+      // {
+      //   path: ':category/:slug',
+      //   component: PageDetailComponent
+      // },
+      // { 
+      //   path: ':category',
+      //   component: ListNewsComponent
+      // },
+      {
+        path: ':category',
+        component: ListNewsComponent,
+        data: {
+          prerender: {
+            entries: getPrerenderParams
+          }
+        }
+      },
       {
         path: ':category/:slug',
-        component: PageDetailComponent
+        component: PageDetailComponent,
+        data: {
+          prerender: {
+            entries: getPrerenderParams
+          }
+        }
       },
-      { 
-        path: ':category',
-        component: ListNewsComponent
-      },
-      { path: '', redirectTo: '/terbaru', pathMatch: 'full' },
-      { path: '**', redirectTo: '/terbaru' }
+      {
+        path: '**',
+        redirectTo: '/terbaru'
+      }
     ]
   }
 ];
